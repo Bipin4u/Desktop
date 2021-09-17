7f8f3beb59dbd858cb317751d123e704d2711056
Bipin4u <88274344+Bipin4u@users.noreply.github.com>
2021-09-17 21:15:37 +0530

    Update abhay2.py

--- abhay2.py
+++ abhay2.py
@@ -1,3 +1,9 @@
+'''program to print second lowest score of the student
+input
+    6
+    4 5 6 4 3 2
+'''
+
 score = input("Give the number of score : ")
 string = input("Enter the score : ")
 list1=string.split()
